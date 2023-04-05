40b0b8c189a7a414e8d4c7aee10c16c2138c7809
Sofiakb <contact.sofiakb@gmail.com>
2023-04-05 15:16:01 +0200

    Updated -- Change export

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,29 @@
  * File src/index
  */
 
+import Api from './api';
+import Model from './core/model';
+import ModelAttribute from './core/model-attribute';
+import HttpController from './core/http.controller';
+import HttpService from './core/http.service';
+import UploadService from './services/upload.api';
+import DownloadService from './services/download.api';
+
 export { __options } from './utils';
 
-export { default as Api } from './api';
-
 export { HttpApi } from './core/http-api';
-export { default as Model } from './core/model';
-export { default as ModelAttribute } from './core/model-attribute';
-export { default as HttpController, AppControllerProperties } from './core/http.controller';
-export { default as HttpService } from './core/http.service';
 
-export { default as UploadService } from './services/upload.api';
-export { default as DownloadService } from './services/download.api';
+export { Api, Model, ModelAttribute, HttpController, HttpService };
+
+export { AppControllerProperties } from './core/http.controller';
+
+export { UploadService, DownloadService };
 
 export { HttpOptions, HttpApiError, ApiAttributes } from './types/api';
 export { GenericModelAttributes } from './types/controller';
 export { ElementType } from './types/element-type';
-export { default as Nullable } from './types/nullable';
 
 export * from './utils';
 export { default as XNumber } from './utils/x-number';
 export { default as DateJs, DateFrom } from './utils/date';
+export { default as Nullable } from './types/nullable';
